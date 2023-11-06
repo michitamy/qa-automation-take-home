9d58e9442bc5f6331527d3240d44107cae66e22f
michelle <michelle.acevedo.t@gmail.com>
2023-11-06 13:55:58 -0800

    Adding test for delete button when list is empty

--- cypress/e2e/Notes.cy.js
+++ cypress/e2e/Notes.cy.js
@@ -11,25 +11,38 @@ describe('Test suite for Notes app', () => {
         {
             expect(response.status).to.eql(200);
         })
+    })
+
+    it('The page is loading correctly',()=>
+    {
         cy.get('h2').should('have.text','QA Automation Cypress Test')
         cy.get('.list-container ul').should('have.length',1)
         cy.get('#text-input').should('exist')
         cy.get("button[type='submit']").should('exist')
         cy.get("button[type='delete']").should('not.exist')
-
     })
-    it("add empty with enter", () => {
+    it("Add element to the list with empty array using enter", () => {
         cy.get('#text-input').click().type('{enter}')
         cy.get('.note-container').should('exist')
         
     })
-    it("add empty using submit button", () => {
+    
+    it("Add element to the list with empty array using submit button", () => {
         cy.get("button[type='submit']").click()
         cy.get('.note-container').should('exist')
         cy.get('.list-container ul div').should('have.class','note-container')
         
     })
-    it("add text using submit button", () => {
+
+    it("Add element to the list with text using enter", () => {
+        cy.get('#text-input').click().type('textDivelement{enter}')
+        cy.get('#text-input').click().type('textDivelement2{enter}')
+        cy.get('.note-container').should('exist')
+        cy.get('.list-container ul div:nth-child(1) li').should('have.have.text',"textDivelement")
+        cy.get('.list-container ul div:nth-child(2) li').should('have.have.text',"textDivelement2")
+        cy.get("div .note-container button").should('have.length',2)
+    })
+    it("Add element to the list with text using submit button", () => {
         cy.get('#text-input').click().type('textDivelement')
         cy.get("button[type='submit']").click()
         cy.get('#text-input').click().type('textDivelement2')
@@ -38,10 +51,13 @@ describe('Test suite for Notes app', () => {
         cy.get('.list-container ul div:nth-child(1) li').should('have.have.text',"textDivelement")
         cy.get('.list-container ul div:nth-child(2) li').should('have.have.text',"textDivelement2")
         cy.get("div .note-container button").should('have.length',2)
-
     })
 
-    it("test the delete button with 1 element", () => {
+    it("Test the delete button with 0 element in the list", () => {
+        cy.get('.list-container ul div').should('have.length',0)
+        cy.get("button[type='delete']").should('not.exist')
+    })
+    it("Test the delete button with 1 element in the list", () => {
         cy.get('#text-input').click().type('textDivelement{enter}')
         cy.get('.list-container ul div').should('have.length',1)
         cy.get('.list-container div button').click()
@@ -50,7 +66,7 @@ describe('Test suite for Notes app', () => {
 
     })
 
-    it("test the delete button with 2 element", () => {
+    it("Test the delete button with 2 element in the list", () => {
         cy.get('#text-input').click().type('textDivelement{enter}')
         cy.get('#text-input').click().type('textDivelement2{enter}')
         cy.get('.list-container ul div').should('have.length',2)
